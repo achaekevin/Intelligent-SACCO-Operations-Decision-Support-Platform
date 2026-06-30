11c975ea119ebf99a3ddd6c1689d5cd83516cfe2
Kevin Achae <achaekevin@gmail.com>
2026-06-30 22:32:45 +0300

    Auto-expose Vite dev server on network

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Expose on network
     port: 5173,
     open: true,
   },
